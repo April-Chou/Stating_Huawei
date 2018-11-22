229e9bc9719e157722def607e89887314b2d6870
cWX636164 <cWX636164@CWX636164-PA.china.huawei.com>
2018-11-22 19:24:59 +0000

    22nd/Nov/2018

--- Starting From Huawei/src/the_50_programming_questions/Question_03.java
+++ Starting From Huawei/src/the_50_programming_questions/Question_03.java
@@ -2,11 +2,23 @@ package the_50_programming_questions;
 
 public class Question_03 {
 	public static void main(String[] args) {
-		for(int i=0;i<=999;i++) {
+		
+		int count = 0;
+		for(int i=100;i<=999;i++) {
 //			hundred place
-			int h = i % 100;
+			int h = i / 100;
+			int t = i % 100 /10;
+			int o = i % 10;
+			
+			if (i ==(int) Math.pow(h, 3) + Math.pow(t, 3) + Math.pow(o, 3)) {
+				System.out.println(i+" is a shui xian hua number");
+				count = count+1;
+			}
+			
 //			
 		}
+		System.out.println("The number of shuixianhua is "+ count);
+	
 	}
 
 }
---
+++ Starting From Huawei/src/the_50_programming_questions/Question_04.java
@@ -0,0 +1,22 @@
+package the_50_programming_questions;
+
+import java.util.Scanner;
+
+public class Question_04 {
+	
+//	prime fraction method
+//	public static int p_fraction(int i){
+//		
+//		return;
+//	}
+//	
+	public void main(String[] args) {
+		int i;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Please enter an intenger");
+		i = in.nextInt();
+		
+		
+	}
+
+}
