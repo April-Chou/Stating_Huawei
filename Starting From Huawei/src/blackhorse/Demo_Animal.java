93319ad7abe64f3fed32055f1ecb72ab1f169f8e
cWX636164 <cWX636164@CWX636164-PA.china.huawei.com>
2018-12-05 14:01:19 +0000

    5th/Dec/2018

---
+++ Starting From Huawei/src/blackhorse/Demo_Animal.java
@@ -0,0 +1,114 @@
+package blackhorse;
+
+
+//  This is the farther class of all animal
+class Animal{
+	private String fur_color;
+	private int leg_number;
+	
+//	construction method
+	public Animal() {
+		
+	}
+	
+	public Animal(String fur_color,int leg_number) {
+		this.fur_color = fur_color;
+		this.leg_number = leg_number;
+		
+	}
+	
+//	set fur color method
+	
+	public void setColor(String fur_color) {
+		this.fur_color = fur_color;
+	}
+	
+//	set leg number method
+	public void setLeg(int leg_number) {
+		this.leg_number = leg_number;
+	}
+	
+//	get fur color method
+	public String getFur() {
+		System.out.print("Fur Color is " + fur_color);
+		return fur_color;
+	}
+	
+//	get leg number method
+	public int getLeg() {
+		System.out.print("The leg number is " + leg_number);
+		return leg_number;
+	}
+	
+//	eat method
+	public void eat() {
+		System.out.println("Eating.......");
+	}
+}
+
+
+
+// son class cat extends animal
+class Cat extends Animal{
+	
+//	construction method
+	public Cat() {
+		
+	}
+	
+	public Cat(String fur, int leg) {
+		super(fur,leg);
+	}
+	
+// overwrite the eat method	
+	public void eat() {
+		System.out.println("The cat is eating fish!");
+	}
+	
+//	catch mouse method 
+	public void catch_mouse() {
+		System.out.println("Catch a mouse!");
+	}
+}
+
+
+
+// son class dog extends animal
+class Dog extends Animal{
+	
+//	Construction Method
+	public Dog() {
+		
+	}
+	
+	public Dog(String fur, int leg_number) {
+		super(fur, leg_number);
+	}
+	
+//	Dog watch method
+	public void watch() {
+		System.out.println("Watch over.....");
+	}
+	
+//	overwrite dog eat method
+	public void eat() {
+		System.out.println("The Dog is eating meat! ");
+	}
+}
+
+
+public class Demo_Animal {
+	public static void main(String[] args) {
+		Cat c1 = new Cat("white", 4);
+		c1.eat();
+		c1.catch_mouse();
+		
+		Dog d1 = new Dog("Yellow", 4);
+		d1.eat();
+		d1.watch();
+		
+		System.out.println("............................");
+		
+	}
+
+}
