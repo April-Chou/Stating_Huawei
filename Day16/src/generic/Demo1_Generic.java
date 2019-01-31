7cc983c5403ae37df0525f5b404e659fe7d13706
April-Chou <zhouchunzhe@gmail.com>
2019-01-31 14:20:53 +0000

    31th/Jan/2019

---
+++ Day16/src/generic/Demo1_Generic.java
@@ -0,0 +1,22 @@
+package generic;
+/*
+ * * A:泛型概述
+* B:泛型好处
+	* 提高安全性(将运行期的错误转换到编译期) 
+	* 省去强转的麻烦
+* C:泛型基本使用
+	* <>中放的必须是引用数据类型 
+* D:泛型使用注意事项
+	* 前后的泛型必须一致,或者后面的泛型可以省略不写(1.7的新特性菱形泛型)  
+	* 
+	* next方法只能调用一次，如果调用多次会将指针向后移动多次泛型最好不定义成object，因为没有任何意义
+	* 
+ */
+public class Demo1_Generic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
