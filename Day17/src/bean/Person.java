e6862a50b2a8288c344544538dee2aa9aaabb8b1
April-Chou <zhouchunzhe@gmail.com>
2019-02-01 12:45:03 +0000

    1st/Feb/2019 before lunch

---
+++ Day17/src/bean/Person.java
@@ -0,0 +1,93 @@
+package bean;
+
+public class Person {
+	private String name;
+	private int age;
+	/**
+	 * 
+	 */
+	public Person() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * @param name
+	 * @param age
+	 */
+	public Person(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + "]";
+	}
+//	/* (non-Javadoc)
+//	 * @see java.lang.Object#equals(java.lang.Object)
+//	 */
+//	@Override
+//	public boolean equals(Object obj) {
+//		// TODO Auto-generated method stub
+//		Person p = (Person) obj;
+//		return this.name.equals(p.name) && this.age == p.age;
+//	}
+//	/* (non-Javadoc)
+//	 * @see java.lang.Object#hashCode()
+//	 */
+//	@Override
+//	public int hashCode() {
+//		final int NUM = 38;
+//		// TODO Auto-generated method stub
+//		return name.hashCode()*NUM + age;
+//	}
+//	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (age != other.age)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+}
--- Day17/src/set/Demo1_HashSet.java
+++ Day17/src/set/Demo1_HashSet.java
@@ -3,12 +3,33 @@ package set;
 import java.util.HashSet;
 /*
  * set集合无索引，不可以重复，无序（存取不一致）
+ * hash code 执行了，equals方法也执行了
  */
+
+import bean.Person;
 
 public class Demo1_HashSet {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//demo1();
+		HashSet<Person> hs = new HashSet<>();
+		hs.add(new Person("Geralt",21));
+		hs.add(new Person("Yennefer",22));
+		hs.add(new Person("Yennefer",22));
+		hs.add(new Person("Ciri",23));
+		hs.add(new Person("Triss",24));
+		hs.add(new Person("DandiLion",25));
+		
+		System.out.println(hs.size());
+		System.out.println(hs);
+
+	}
+
+	/**
+	 * 
+	 */
+	private static void demo1() {
 		HashSet<String> hs = new HashSet<>();
 		boolean b1 = hs.add("a");
 		boolean b2 = hs.add("a");
@@ -24,7 +45,6 @@ public class Demo1_HashSet {
 		for(String i : hs) {
 			System.out.println(i);
 		}
-
 	}
 
 }
