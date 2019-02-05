ced545df8ad949536dbf08531e95080c89d2982e
April-Chou <zhouchunzhe@gmail.com>
2019-02-05 12:41:23 +0000

    no idea  niming neibu lei

---
+++ Day17/src/test/Test4.java
@@ -0,0 +1,28 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.TreeSet;
+
+public class Test4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> list = new ArrayList<>();
+		list.add("aaa");
+		list.add("aaa");
+		list.add("ccc");
+		list.add("cba");
+		list.add("nba");
+		list.add("witcher");
+		list.add("geralt");
+		list.add("geralt");
+		
+
+	}
+	
+//	public static int sort(ArrayList<String> list) {
+//		TreeSet<String> ts = new TreeSet<>(new Comparator<String>());
+//		return 0;
+//	}
+
+}
